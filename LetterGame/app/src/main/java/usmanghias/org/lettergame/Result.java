032d922c4a717ca3b066cdbc1d4c6c58fec8c7f5
Muhammad Usman Ghias Ali <99526697+UsmanGhias@users.noreply.github.com>
2023-07-11 14:16:21 +0500

    Added Result.java File
    Added results and all the required features are updtaed

--- LetterGame/app/src/main/java/usmanghias/org/lettergame/Result.java
+++ LetterGame/app/src/main/java/usmanghias/org/lettergame/Result.java
@@ -1,4 +1,56 @@
 package usmanghias.org.lettergame;
 
-public class Result {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ResultActivity extends AppCompatActivity {
+
+    TextView result, point;
+    DbHelper db;
+    Button home;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        home = findViewById(R.id.button3);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(ResultActivity.this, DrawerActivity.class);
+                startActivity(intent2);
+            }
+        });
+
+        db = new DbHelper(ResultActivity.this);
+
+        List<String> ans = db.getResult();
+
+        String answer = ans.get(0);
+        String correct = ans.get(1);
+        String ques = ans.get(2);
+
+        int points = 0;
+        for (int i = 0; i < 5; i++) {
+            if (answer.charAt(i) == correct.charAt(i)) {
+                points += 1;
+            }
+        }
+
+        result = findViewById(R.id.result);
+        result.setText("Letter: " + ques.charAt(0) + "\t Answer Given: " + answer.charAt(0) + "\t Correct Answer: " + correct.charAt(0) + "\n" +
+                "Letter: " + ques.charAt(1) + "\t Answer Given: " + answer.charAt(1) + "\t Correct Answer: " + correct.charAt(1) + "\n" +
+                "Letter: " + ques.charAt(2) + "\t Answer Given: " + answer.charAt(2) + "\t Correct Answer: " + correct.charAt(2) + "\n");
+
+        point = findViewById(R.id.point);
+        point.setText(points + "/5");
+    }
 }
